48138340b0fe16592dba5b8bc7ac0d0cf9e70f57
Douglas Roeder <douglas.roeder@jora.com>
2018-08-05 18:13:37 +1000

    more tests

--- tests/categories_controller_test.go
+++ tests/categories_controller_test.go
@@ -34,3 +34,25 @@ func (suite *TestSuite) TestCategoriesController_Index() {
 	suite.Equal(2, len(categories))
 	suite.Equal("Laptops", categories[0].Name)
 }
+
+func (suite *TestSuite) TestCategoriesController_Show() {
+	service := services.NewCategoryService(goWork.DB)
+	controller := controllers.NewCategoriesController(service)
+
+	w := httptest.NewRecorder()
+	router := gin.Default()
+	router.GET("/show/:id", controller.Show)
+
+	req, _ := http.NewRequest("GET", "/show/1", nil)
+	router.ServeHTTP(w, req)
+
+	suite.Equal(200, w.Code)
+
+	var category models.Category
+	err := json.Unmarshal(w.Body.Bytes(), &category)
+	if err != nil {
+		suite.Fail("Error parsing JSON response")
+	}
+
+	suite.Equal("Laptops", category.Name)
+}
