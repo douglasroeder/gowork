80627673457b427ca7bac140b686029d3a15aa07
Douglas Roeder <douglas.roeder@jora.com>
2018-08-13 22:24:01 +1000

    added more contact service helpers

--- services/contact.go
+++ services/contact.go
@@ -10,6 +10,9 @@ import (
 // ContactService handles some of the CRUD operations
 type ContactService interface {
 	GetAll() []models.Contact
+	GetByID(id int64) (models.Contact, bool)
+	Insert(category *models.Contact) (bool, []string)
+	DeleteByID(id int64) bool
 }
 
 // NewContactService returns and instance of ContactService
@@ -31,4 +34,43 @@ func (s *contactService) GetAll() []models.Contact {
 	}
 
 	return contacts
+}
+
+func (s *contactService) GetByID(id int64) (models.Contact, bool) {
+	var contact models.Contact
+
+	if err := s.db.Where("id = ?", id).First(&contact).Error; err != nil {
+		fmt.Println("Error")
+
+		return contact, false
+	}
+
+	return contact, true
+}
+
+func (s *contactService) DeleteByID(id int64) bool {
+	var contact models.Contact
+
+	s.db.Where("id = ?", id).Delete(&contact)
+
+	return true
+}
+
+func (s *contactService) Insert(contact *models.Contact) (bool, []string) {
+	if s.db.NewRecord(*contact) {
+		result := s.db.Create(contact)
+
+		errors := []string{}
+		for _, error := range result.GetErrors() {
+			errors = append(errors, error.Error())
+		}
+
+		if len(errors) >= 1 {
+			return false, errors
+		}
+
+		return true, errors
+	}
+
+	return false, []string{}
 }
--- services/contact_test.go
+++ services/contact_test.go
@@ -1,8 +1,47 @@
 package services
 
+import "github.com/douglasroeder/gowork/models"
+
 func (suite *TestSuite) TestContactService_GetAll() {
 	service := NewContactService(goWork.DB)
 
 	contacts := service.GetAll()
 	suite.Equal(2, len(contacts))
+}
+
+func (suite *TestSuite) TestContactService_GetByIDWhenValid() {
+	service := NewContactService(goWork.DB)
+
+	contact, found := service.GetByID(1)
+	suite.Equal(true, found)
+	suite.Equal("John Doe", contact.Name)
+}
+
+func (suite *TestSuite) TestContactService_GetByIDWhenNotFound() {
+	service := NewContactService(goWork.DB)
+
+	_, found := service.GetByID(9999)
+	suite.False(found)
+}
+
+func (suite *TestSuite) TestContactService_Insert() {
+	service := NewContactService(goWork.DB)
+	contact := models.Contact{
+		Name: "Mary Ann",
+	}
+
+	success, _ := service.Insert(&contact)
+
+	suite.True(success)
+	suite.NotNil(contact.ID)
+}
+
+func (suite *TestSuite) TestContactService_DeleteByID() {
+	service := NewContactService(goWork.DB)
+
+	deleted := service.DeleteByID(1)
+	suite.True(deleted)
+
+	_, found := service.GetByID(1)
+	suite.False(found)
 }
